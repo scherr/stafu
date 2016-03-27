f1a5401e9c85176989acdc67fe55a5c550ee275e
scherr <scherr@csg.ci.i.u-tokyo.ac.jp>
2016-03-27 23:40:51 +0900

    Experiments with making things more generic...

--- src/stafu/example/Power.java
+++ src/stafu/example/Power.java
@@ -11,6 +11,12 @@ public final class Power {
         return stafu.Statification.statify(f);
     }
 
+    @FunctionalInterface
+    private interface ToIntFunction<T> extends java.util.function.ToIntFunction<T>, Serializable { }
+    private static <T> ToIntFunction<T> statify2(ToIntFunction<T> f) {
+        return stafu.Statification.statify(f);
+    }
+
     private static int power(int x, int y) {
         if (y == 0) {
             return 1;
@@ -38,11 +44,28 @@ public final class Power {
         }
     }
 
+    private static <T> ToIntFunction<T> powerGen2(ToIntFunction<T> x, int y) {
+        if (y == 0) {
+            return statify2(env -> 1);
+        } else if (y == 1) {
+            return x;
+        } else if (y % 2 == 0) {
+            ToIntFunction<T> t = powerGen2(x, y / 2);
+            return statify2(env -> { int i = t.applyAsInt(env); return i * i; });
+        } else {
+            ToIntFunction<T> t = powerGen2(x, y - 1);
+            return statify2(env -> x.applyAsInt(env) * t.applyAsInt(env));
+        }
+    }
+
     public static void main(String[] args) {
         IntUnaryOperator power45;
 
         power45 = x -> power(x, 45);
         power45 = powerGen(statify((x -> x)), 45);
+
+        ToIntFunction<int[]> temp = powerGen2(statify2(x -> x[0]), 45);
+        power45 = statify(x -> temp.applyAsInt(new int[]{x}));
 
         int res = 0;
         for (int i = 0; i < 1000000000; i++) {
